558d6298d2b75da8c932777fb2d75f6e96195233
Chris <jeky.tck@gmail.com>
2023-03-30 21:43:03 +0100

    Initialisation of first classes

--- Task2/src/task2/ApplicationRunner.java
+++ Task2/src/task2/ApplicationRunner.java
@@ -1,7 +1,15 @@
 
 package task2;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class ApplicationRunner {
+
+    private List<Member> members;
+    private List<Instructor> instructors;
+    private List<Lesson> lessons;
 
     public static void main(String[] args) {
         
