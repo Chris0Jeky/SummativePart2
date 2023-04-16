8a18a65980a1e83a81d94236b77f76956a3f6c54
Chris <jeky.tck@gmail.com>
2023-04-16 22:49:17 +0100

    Added bubble sort to sort Object[][] based on their position [][1](numerically)

--- Task1/src/task1/ApplicationRunner.java
+++ Task1/src/task1/ApplicationRunner.java
@@ -1,5 +1,6 @@
 package task1;
 
+import java.util.Arrays;
 import java.util.Vector;
 
 public class ApplicationRunner {
@@ -8,18 +9,27 @@ public class ApplicationRunner {
     public static void main(String[] args) {
         FileReader read = new FileReader();
         //read.printRead();
+        /*
         Vector<String> vecty = read.read();
         System.out.println(vecty.get(0));
+        System.out.println(vecty.get(1));
         System.out.println(createSportsAward(vecty.get(0)));
         Object[] ar = ObjectBuilder.mkArrayOfData(vecty.get(0));
+
         System.out.println(ar[0]);
-        System.out.println(ar[1].toString());
-        System.out.println(ar[2].toString());
+        System.out.println(((SportsPerson)ar[1]).getName());
+        System.out.println(((SportsPerson)ar[2]).getSport());
         System.out.println(ar[3].toString());
         System.out.println(ar[4].toString());
-        System.out.println(ar[5].toString());
+        System.out.println(ar[5].toString()); */
 
-        Menu.mainMenuLoop();
+       // Menu.mainMenuLoop();
+
+
+        int[][] test = {{1,2,3},{4,5,6},{7,8,9},{7,8,9},{7,8,9},{7,8,9}};
+        System.out.println(test.length);
+        int[][] test2 = new int[6][3];
+        System.out.println(test2[2].length);
 
     }
     /*1995|Jonathan Edwards (ENG),Athletics|Frank Bruno (ENG),Boxing|Colin McRae (SCO),Rallying|Ryder Cup team (EU),Golf,Bernard Gallacher
@@ -27,6 +37,24 @@ public class ApplicationRunner {
     1997|Greg Rusedski (ENG),Tennis|Tim Henman (ENG),Tennis|Steve Redgrave (ENG),Rowing|British & Irish Lions squad (UK),Rugby union,Martin Johnson
 */
 
+    public static Object[][] bubbleSort (Object[][] infoToSort){
+
+        //Object[][] sortedInfo = new Object[infoToSort.length][5];
+        System.out.println(Arrays.toString(infoToSort));
+        Object[] temp = new Object[5];
+        for (int i = 0; i < infoToSort.length; i++){
+            for (int j = 0; j < infoToSort.length; j++){
+                if (((Integer)infoToSort[i][1]) > ((Integer)infoToSort[j][1])){
+                    temp = infoToSort[i];
+                    infoToSort[i] = infoToSort[j];
+                    infoToSort[j] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(infoToSort));
+        return infoToSort;
+
+    }
     public static SportsAward createSportsAward(String data){
 
         ObjectBuilder.mkArrayOfData(data);
